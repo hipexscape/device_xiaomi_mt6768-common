661a75c2c06a50319fecfd1bc34ac4a283698095
bengris32 <bengris32@protonmail.ch>
2023-11-03 10:19:17 +0100

    mt6768-common: Switch to AOSP audio service
    Change-Id: I933b66e3821562022eb837af91ad4039e22fb578
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service.mediatek \
+    android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.bluetooth.audio-impl
