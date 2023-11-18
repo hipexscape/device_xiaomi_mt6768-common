b5f33f1ec9ad3f35842aab24dedff662d326be61
bengris32 <bengris32@protonmail.ch>
2023-11-18 01:17:23 +0000

    mt6768-common: Switch to AIDL ConsumerIr/RIL service
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -186,8 +186,7 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.mediatek
 
 # Enable DM file pre-opting to reduce first boot time
 PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
