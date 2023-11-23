242228789ffa4502fc69b0a8e1dc7efab3458c6c
alibei <sebastiankotzias83@gmail.com>
2023-11-23 16:12:41 +0000

    mt6768-common: Silence SpeechMessengerNormal logspam SpeechMessengerNormal: openCcciDriver(), open(/dev/ccci_aud) failgit add . mCcciDeviceHandler: -1, errno: 2
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -277,4 +277,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.MAPI-SocketListener=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MAPI-CommandProcessor=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.SpeechMessenger=$(VENDOR_LOG_LEVEL)
