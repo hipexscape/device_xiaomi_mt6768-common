145861eb92adb42e6642d3b6204e4db74ca27548
Sarthak Roy <sarthakroy2002@gmail.com>
2024-02-15 14:06:43 +0530

    mt6768-common: Build `android.frameworks.sensorservice@1.0.vendor`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iff9eaf0b79476e2b86aa21154f8b4030bbe1d16b

--- mt6768.mk
+++ mt6768.mk
@@ -368,7 +368,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libshim_sensors \
