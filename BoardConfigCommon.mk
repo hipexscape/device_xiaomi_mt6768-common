e824f6e89a3615e570b76ef83efaa744aebbbf39
SamarV-121 <samar@samarv121.dev>
2024-06-16 10:59:16 +0530

    mt6768-common: Include device FCM for mediatek HALs
    Change-Id: I1a908cbd63641335eb5fb6cdd6c34925ae10302e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,7 +168,9 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+$(COMMON_PATH)/framework_compatibility_matrix.xml
 
 # Wi-Fi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
