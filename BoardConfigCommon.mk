6b99489eec893431a634f10309d6613be5dbc764
Georg Veichtlbauer <georg@vware.at>
2024-09-15 08:33:29 +0530

    mt6768-common: powerhint: Add DT2W
    Change-Id: Ia012dd8f8c0610287301f1ad5fadde01a44c2449

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,9 +111,6 @@ TARGET_BOARD_PLATFORM := mt6768
 BOARD_HAS_MTK_HARDWARE := true
 BOARD_HAVE_MTK_FM := true
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
