431ebc0220d88069e5d618aab590003fc27d69e3
Abhinav Kumar <abhinav.115260@gmail.com>
2025-02-11 20:35:44 +0530

    Revert "mt6768-common: Build common Xiaomi Light service"
    This reverts commit a4a7d7503b79431a913740818b5e5e7ba33fd6c9.

--- mt6768.mk
+++ mt6768.mk
@@ -256,7 +256,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.mt6768
 
 # Lineage Health
 ifneq ($(wildcard hardware/*/interfaces/health),)
